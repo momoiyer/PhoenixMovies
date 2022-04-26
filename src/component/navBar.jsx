b14f9a03884d827d8ceb1a44ed22eb4e5ab6509c
momo <khinmomozin@gmail.com>
2022-04-26 15:33:27 -0400

    nav bar changes

--- src/component/navBar.jsx
+++ src/component/navBar.jsx
@@ -12,8 +12,8 @@ function NavBar(props) {
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
             <NavLink className="nav-item nav-link" to="/movies">Movies</NavLink>
-            <NavLink className="nav-item nav-link" to="/customers">Customers</NavLink>
-            <NavLink className="nav-item nav-link" to="/rentals">Rentals</NavLink>
+            {/* <NavLink className="nav-item nav-link" to="/customers">Customers</NavLink> */}
+            {/* <NavLink className="nav-item nav-link" to="/rentals">Rentals</NavLink> */}
             <NavLink className="nav-item nav-link" to="/login">Login</NavLink>
             <NavLink className="nav-item nav-link" to="/register">Register</NavLink>
         </div>
