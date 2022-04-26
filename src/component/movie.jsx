e660e1465264888a10d46ad8fa4d3821c1b885c6
momo <khinmomozin@gmail.com>
2022-04-26 12:39:51 -0400

    search function

--- src/component/movie.jsx
+++ src/component/movie.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import _ from 'lodash';
 import Pagination from './common/pagination.jsx';
 import ListGroup from './common/listGroup.jsx';
 import MoviesTable from './moviesTable.jsx';
 import { getMovies } from '../services/fakeMovieService.js';
 import { getGenres } from '../services/fakeGenreService.js';
 import { paginate } from '../utils/paginate.js';
-import _ from 'lodash';
+import Input from './common/input.jsx';
 
     
 function Movie(props) {
@@ -18,7 +19,8 @@ function Movie(props) {
     const [pageSize, setPageSize] = useState(0);
     const [selectedGenre, setSelectedGenre] = useState('');
     const [selectedMoive, setSelectedMovie] = useState([]);
-    const [sortColumn, setSortColumn] = useState({path: 'title', order: 'asc'});
+    const [sortColumn, setSortColumn] = useState({ path: 'title', order: 'asc' });
+    const [searchQuery, setSearchQuery] =  useState('');
 
     useEffect(() => {
         setPageSize(4);
@@ -33,9 +35,10 @@ function Movie(props) {
         setSelectedMovie(movies);
     }    
 
-    const updateGenreSelection = (movies,genre) => {
+    const updateGenreSelection = (movies, genre) => {
         setSelectedMovie(movies);
         setSelectedGenre(genre);
+        setCurrentPage(1);
     }
 
     const handleDelete = (movie) => {
@@ -58,14 +61,27 @@ function Movie(props) {
         return paginate(sortedMovie, currentPage, pageSize);
     }
 
-    const handleGenreSelect = (genre) => {
-        setCurrentPage(1);
+    const handleGenreSelect = (genre) => {        
+        setSearchQuery('');
         if (!genre._id) return updateGenreSelection(movies, genre);
         if (selectedGenre !== genre) return updateGenreSelection(movies.filter(m => m.genre._id === genre._id), genre);
     }
 
     const handleSort = sortedColumn => {        
         setSortColumn(sortedColumn);
+    }
+
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+        
+        updateGenreSelection(movies, null);
+        
+        if (query) {
+            const searchedMovies = movies.filter(m => {
+                if (m.title.toLowerCase().includes(query.toLowerCase())) return m;
+            });
+            setSelectedMovie(searchedMovies);
+        }        
     }
 
     const sortedMovie = _.orderBy(selectedMoive, [sortColumn.path], [sortColumn.order]);
@@ -91,6 +107,12 @@ function Movie(props) {
                             New Movie
                         </Link>
                         <p>Showing {selectedMoive.length} movies in the database </p>
+                        
+                        <Input
+                            value={searchQuery}
+                            placeholder="Search..."
+                            onChange={e=> handleSearch(e.currentTarget.value)}
+                        />
                         <MoviesTable
                             movies={paginateMovies()}
                             sortColumn={sortColumn}
